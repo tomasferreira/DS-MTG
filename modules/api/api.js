ba220d459b0f4ef1442a8d7d94295f486051fbfc
Tomas Ferreira <tomasferreira@MacBook-Pro.local>
2020-06-26 14:12:43 +0200

    add

--- modules/api/api.js
+++ modules/api/api.js
@@ -1,5 +1,26 @@
-module.exports = Class.create({
+'esversion: 6';
+var HTTPScriptable = require("core/HTTPScriptable");
+
+export default Class.create({
 	sync: function(id) {
 		console.log('hello sync');
+		
+
+		// let response = h.get();
+        // console.log("query got HTTP code: " + response.getResponseCode());
+        // let responseObj = JSON.parse(response.getResponseBody()).cards;
+        // let records = [];
+        // if(!responseObj) return records;
+		// let counter = 1;
+        // responseObj.forEach(message => {
+        //     let record = {
+        //         id: counter,
+        //     };
+        //     records.push(record);
+		// 	counter++;
+        // });
+        // return records;
+
+
 	}
 });
