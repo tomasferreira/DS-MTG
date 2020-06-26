69ccae5dc099e88c38f75ab39eb2d0eaaff820f4
Tomas Ferreira <tomasferreira@MacBook-Pro.local>
2020-06-26 17:08:28 +0200

    add

--- modules/api/api.js
+++ modules/api/api.js
@@ -3,21 +3,27 @@ var HTTPScriptable = require("core/HTTPScriptable");
 
 module.exports = Class.create({
 	syncAll: function() {
-		var h = new HTTPScriptable('https://www.mtgjson.com/json/RAV.json');
+		var h = new HTTPScriptable('https://www.mtgjson.com/json/AllCards.json');
         let response = h.get();
         console.log("query got HTTP code: " + response.getResponseCode());
-        // let responseObj = JSON.parse(response.getResponseBody());
-        // let records = [];
-        // if(!responseObj) return records;
-        // responseObj.forEach(message => {
-        //     let record = {
-        //         id: message.Id,
-        //         message: message.Message ? message.Message : '',
-        //         creator: message.CreatorName ? message.CreatorName : '',
-        //         created: message.CreateOn ? Date.parse(message.CreateOn) : '',
-        //     };
-        //     records.push(record);
-        // });
-        // return records;
+
+        let responseObj = JSON.parse(response.getResponseBody());
+        if(!responseObj) return;
+
+        
+        for (const cardName in responseObj) {
+            if (responseObj.hasOwnProperty(cardName)) {
+                const card = responseObj[cardName];
+
+                var cardRecord = new FRecord('card');
+
+                cardRecord.uuid = card.uuid;
+                cardRecord.name = card.name;
+
+                cardRecord.insert();
+                
+            }
+        }
+        return records;
 	}
 });
